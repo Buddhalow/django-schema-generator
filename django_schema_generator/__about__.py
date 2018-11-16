d03991bd6857ca784367e996995d0fc5e45537e3
Alexander Forselius <alexander.forselius@buddhalow.com>
2018-11-16 11:46:15 +0100

    Added info

--- django_schema_generator/__about__.py
+++ django_schema_generator/__about__.py
@@ -1,8 +1,11 @@
-__package_name__ = 'django-admin-generator'
-__version__ = '2.0.0'
-__author__ = 'Rick van Hattem'
-__author_email__ = 'Wolph@Wol.ph'
-__description__ = ('Django Admin Generator is a management command to '
-'automatically generate a Django `admin.py` file for given apps/models.')
-__url__ = 'https://github.com/WoLpH/django-admin-generator/'
+__package_name__ = 'django-schema-generator'
+__version__ = '1.0.0'
+__author__ = 'Rick van Hattem & Alexander Forselius'
+__author_email__ = 'alexander.forseliu@buddhalow.com'
+__description__ = (
+    'Django Schema Generator is a management command to '
+    'automatically generate a Django `schema.py` file for given apps/models.'
+    ' Based on django-admin-generator by Rick van Mattem'
+)
+__url__ = 'https://github.com/Buddhalow/django-schema-generator/'
 
